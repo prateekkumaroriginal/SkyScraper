4d3542530a8e4b372a181ec10541e6ccbceb5404
Prateek <prateek.kumar.original@gmail.com>
2023-11-24 23:27:23 +0530

    Create link model

--- sky_scraper/scraper/views.py
+++ sky_scraper/scraper/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from .models import Link
 
 # Create your views here.
 
 def scrape(request):
     page = requests.get('https://google.com')
     soup = BeautifulSoup(page.text, 'html.parser')
-    links = []
-    for a in soup.find_all('a'):
-        links.append(a.get('href'))
     
+    for a in soup.find_all('a'):
+        address = a.get('href')
+        name = a.text.strip()
+        Link.objects.create(address=address, name=name)
+    
+    links = Link.objects.all()
+    if links:
+        Link.objects.all().delete()
+        
     return render(request, 'scraper/result.html', {'links':links})
